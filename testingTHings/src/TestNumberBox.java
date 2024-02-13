d835f38addd12bd27087623a15ad8f375172b176
a-s-thomas <asthomas.8.30@gmail.com>
2024-02-12 18:56:12 -0700

    Test commit

---
+++ testingTHings/src/TestNumberBox.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TestNumberBox {
+    public static void main(String[] args) {
+        //get rows and columns
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many rows do you want?: ");
+        int rows = in.nextInt();
+        System.out.println("How many columns do you want?: ");
+        int columns = in.nextInt();
+        //prints rows and columns
+        printRows(rows, columns);
+
+    }
+
+    //print number x times based on columns requested
+    public static void getColumns(int columnNum, int number) {
+        for (int count = number + 1; count < columnNum; count += 1) {
+            System.out.print(count + " ");
+        }
+    }
+
+    //print columns into rows based on rows requested
+    public static void printRows(int rowNum, int columnVar) {
+        for (int counter = 0; counter < rowNum; counter += 1) {
+            System.out.println();
+            columnVar += 1;
+            getColumns(columnVar,counter);
+
+        }
+    }
+}
